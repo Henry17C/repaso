556da2fab7966f6f756ce991f4ba566a4bd6e4cb
Henry Coyago <https://github.com/Henry17C/repaso.git>
2022-11-20 23:20:45 -0500

    repaso

---
+++ src/main/java/com/example/demo/tradicional/Medico.java
@@ -0,0 +1,20 @@
+package com.example.demo.tradicional;
+
+public class Medico {
+	private String nombre;
+	private String cedula;
+	//set get
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+}
